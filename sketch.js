ce4ec717cf7518678050c32c89973ef31d2f5e87
Nguyen Trung Long <somethings.ttt@gmail.com>
2018-01-23 15:36:37 +0700

    Commit from School computer to check git's connection

--- sketch.js
+++ sketch.js
@@ -5,135 +5,98 @@
 */
 
 
-var sub = 5, 
-    now = 1, 
-    IdList, 
-    up, 
-    down, 
-    width = 10, 
-    height = 10, 
-    _temp, 
+var sub = 5,
+    now = 1,
+    IdList,
+    up,
+    down,
+    width = 10,
+    height = 10,
+    _temp,
     setTimer = 20000,
-    timer = setTimer, 
-    xName = "X Player", 
-    oName = "O Player", 
+    timer = setTimer,
+    xName = "X Player",
+    oName = "O Player",
     gameOver = false,
     playing = false,
     count = 0,
     xSource = "theX.png",
     oSource = "theY.png";
 
-function DrawTable()
-{
+function DrawTable() {
     var draw = "<tr><td class='td'>";
-    for(var i = 1; i < width; i++) draw += "</td><td class='td'>";
+    for (var i = 1; i < width; i++) draw += "</td><td class='td'>";
     draw += "</td></tr>";
     _temp = draw;
-    for(var i = 1; i < height; i++) draw += _temp;
+    for (var i = 1; i < height; i++) draw += _temp;
     IdList = new Array(width * height);
     return draw;
 }
 
 //#region Check
-function IsEndVertical(index)
-{
+function IsEndVertical(index) {
     up = 1;
     down = 0;
     _temp = index;
-    while(_temp > width && IdList[_temp - width] == IdList[_temp])
-    {
-        up += 1;
-        _temp -= width;
-    }
+    while (_temp > width && IdList[_temp - width] == IdList[_temp])
+        up += 1, _temp -= width;
     _temp = index;
-    while(_temp < width * height - 1 && IdList[_temp + width] == IdList[_temp])
-    {
-        down += 1;
-        _temp += width;
-    }
-    if(up + down >= sub) return true;
+    while (_temp < width * height - 1 && IdList[_temp + width] == IdList[_temp])
+        down += 1, _temp += width;
+    if (up + down >= sub) return true;
     else return false;
 }
 
-function IsEndHorizontal(index)
-{
+function IsEndHorizontal(index) {
     up = 1;
     down = 0;
     _temp = index;  //Reset variables 
-
-    while(_temp > parseInt(index / 10) * 10 && IdList[_temp - 1] == IdList[_temp])
-    {
-        up += 1;
-        _temp -= 1;
-    }
-
+    while (_temp > parseInt(index / 10) * 10 && IdList[_temp - 1] == IdList[_temp])
+        up += 1, _temp -= 1;
     _temp = index;
-
-    while(_temp < (parseInt(index / 10) + 1) * 10 && IdList[_temp + 1] == IdList[_temp])
-    {
-        down += 1;
-        _temp += 1;
-    }
-
-    if(up + down >= sub) return true;
+    while (_temp < (parseInt(index / 10) + 1) * 10 && IdList[_temp + 1] == IdList[_temp])
+        down += 1, _temp += 1;
+    if (up + down >= sub) return true;
     else return false;
 }
 
-function IsEndRightUp(index)
-{
+function IsEndRightUp(index) {
     up = 1;
     down = 0;
     _temp = index;
-    while(index % width != width && IdList[_temp] == IdList[_temp - width + 1])
-    {
-        up += 1;
-        _temp -= (width - 1);
-    }
+    while (index % width != width && IdList[_temp] == IdList[_temp - width + 1])
+        up += 1, _temp -= (width - 1);
     _temp = index;
-    while(index % width != width && IdList[_temp] == IdList[_temp + width - 1])
-    {
-        up += 1;
-        _temp += (width - 1);
-    }
-
-    if(up + down >= sub) return true;
+    while (index % width != width && IdList[_temp] == IdList[_temp + width - 1])
+        up += 1, _temp += (width - 1);
+    if (up + down >= sub) return true;
     else return false;
 }
 
-function IsEndRightDown(index)
-{
+function IsEndRightDown(index) {
     up = 1;
     down = 0;
     _temp = index;
-    while(index % width != width && IdList[_temp] == IdList[_temp - width - 1])
-    {
-        up += 1;
-        _temp -= (width + 1);
-    }
+    while (index % width != width && IdList[_temp] == IdList[_temp - width - 1])
+        up += 1, _temp -= (width + 1);
     _temp = index;
-    while(index % width != width && IdList[_temp] == IdList[_temp + width + 1])
-    {
-        up += 1;
-        _temp += (width + 1);
-    }
-
-    if(up + down >= sub) return true;
+    while (index % width != width && IdList[_temp] == IdList[_temp + width + 1])
+        up += 1, _temp += (width + 1);
+    if (up + down >= sub) return true;
     else return false;
 }
 
-function IsGameOver(index)
-{
-    if(IsEndVertical(index)
-    || IsEndHorizontal(index)
-    || IsEndRightUp(index)
-    || IsEndRightDown(index)) return true;
+function IsGameOver(index) {
+    if (IsEndVertical(index)
+        || IsEndHorizontal(index)
+        || IsEndRightUp(index)
+        || IsEndRightDown(index)) return true;
     else return false;
 }
 //#endregion
 
 //#region Event 
-function GameOver()
-{
+function GameOver() {
     gameOver = true;
     count += 1;
     var winner = now == 1 ? oName : xName;
@@ -143,26 +106,24 @@ function GameOver()
     $(".history-append").append("<tr><td class='center'>" + count + "</td><td class='center'>" + winner + "</td></tr>");
 }
 
-function Update()
-{
+function Update() {
     $(".canvas").width($(".table").width());
     $(".canvas").height($(".table").height());
     $(".canvas").css("top", $(".table").css("top"));
     $(".showName").text(now == 1 ? xName : oName);
-    $(".col-3").css({"display": $(window).width() <= 980 ? "none" : "block"});
+    $(".col-3").css({ "display": $(window).width() <= 980 ? "none" : "block" });
     $(".td").css("width", 500 / (width >= height ? width : height) + "px");
     $(".td").css("height", 500 / (width >= height ? width : height) + "px");
     $(".showImg").attr("src", now == 1 ? xSource : oSource);
     $(".showName").text(now == 1 ? xName : oName);
 }
 
-function Reset()
-{
+function Reset() {
     $(".table").css("display", "block");
     $(".winner").css("display", "none");
     $(".pause").css("display", "none");
     $(".table").html(DrawTable());
-    for(var i = 0; i < IdList.length; i++)
+    for (var i = 0; i < IdList.length; i++)
         IdList[i] = null;
     gameOver = false;
     playing = true;
@@ -170,78 +131,75 @@ function Reset()
 }
 //#endregion
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".table").html(DrawTable());
 
     //#region JS events
-    $("table").on("click", ".td", function(){
+    $("table").on("click", ".td", function () {
         Update();
         var index = $("td").index(this);
         timer = setTimer;
-        if(now == 1 && $(this).attr("id") != "checked")
-        {
+        if (now == 1 && $(this).attr("id") != "checked") {
             $(this).css("background-image", "url(" + xSource + ")");
             now = 0;
             $(this).attr("id", "checked");
             IdList[index] = "X";
         }
-        if(now == 0 && $(this).attr("id") != "checked")
-        {
+        if (now == 0 && $(this).attr("id") != "checked") {
             $(this).css("background-image", "url(" + oSource + ")");
             now = 1;
             $(this).attr("id", "checked");
             IdList[index] = "O";
         }
-        if(IsGameOver(index))
-        {
+        if (IsGameOver(index)) {
             GameOver();
         }
     });
-    $(".btn-reset").click(function(){
+    $(".btn-reset").click(function () {
         Reset();
     });
-    $(".btn-pause").click(function(){
+    $(".btn-pause").click(function () {
         $(".pause").css("display", "flex");
         $(".table").css("display", "none");
         playing = false;
     });
-    $(".btn-continue").click(function(){
+    $(".btn-continue").click(function () {
         $(".pause").css("display", "none");
         $(".table").css("display", "block");
         playing = true;
     });
-    $(".btn-new-start").click(function(){
+    $(".btn-new-start").click(function () {
         $(".container").css("display", "flex");
         $(".start").css("display", "none");
         playing = true;
     });
-    $(".btn-setting").click(function(){
+    $(".btn-setting").click(function () {
         $(".container").css("display", "none");
         $(".start").css("display", "none");
         $(".settings").css("display", "flex");
     });
-    $(".btn-out-setting").click(function(){
+    $(".btn-out-setting").click(function () {
         $(".settings").css("display", "none");
     });
-    $("#step").change(function(){
+    $("#step").change(function () {
         $(".show-step").text($(this).val());
     })
-    $("#set-width").change(function(){
+    $("#set-width").change(function () {
         $(".show-width").text($(this).val());
     });
-    $("#set-height").change(function(){
+    $("#set-height").change(function () {
         $(".show-height").text($(this).val());
     });
-    $("#set-timer").change(function(){
+    $("#set-timer").change(function () {
         $(".show-timer").text($(this).val());
     });
-    $("#set-xname").change(function(){
+    $("#set-xname").change(function () {
         $(".show-xname").text($(this).val());
     });
-    $("#set-oname").change(function(){
+    $("#set-oname").change(function () {
         $(".show-oname").text($(this).val());
     });
-    $(".out-setting").click(function(){
+    $(".out-setting").click(function () {
         $("#set-width").val(width);
         $("#set-height").val(height);
         $("#step").val(sub);
@@ -255,9 +213,9 @@ $(document).ready(function(){
         $(".show-oname").text(oName);
         $(".show-step").text(sub);
         $(".settings").css("display", "none");
-        $(".start").css("display", "flex"); 
+        $(".start").css("display", "flex");
     });
-    $(".new-game-fromstart").click(function(){
+    $(".new-game-fromstart").click(function () {
         width = $("#set-width").val();
         height = $("#set-height").val();
         sub = $("#step").val();
@@ -269,13 +227,12 @@ $(document).ready(function(){
         Reset();
     });
     //#endregion
-    setInterval(function(){
+    setInterval(function () {
         Update();
-        if(timer <= 0)
-        {
+        if (timer <= 0) {
             timer = setTimer;
             now = now == 1 ? 0 : 1;
-        } 
+        }
         $(".showTimer").text("Time left: " + timer / 1000 + (timer % 1000 == 0 ? ".0" : ""));
         timer -= gameOver || !playing ? 0 : 100;
     }, 100)
